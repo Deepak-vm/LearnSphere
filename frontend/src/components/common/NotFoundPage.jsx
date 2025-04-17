225fee0079ce859dfa78cabd40a70e5a65b9503b
Deepak-vm <18vdeepak@gmail.com>
2025-04-17 13:32:53 +0530

    Page not found

--- frontend/src/components/common/NotFoundPage.jsx
+++ frontend/src/components/common/NotFoundPage.jsx
@@ -6,7 +6,7 @@ export default function NotFoundPage() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <img src={NotFoundImage} alt="404 Not Found" className="max-w-md" />
       <h1 className="text-2xl font-bold mt-4">Oops! Something Went Wrong.</h1>
-      <p className="text-gray-600 mt-2">Error 404: Page Not Found</p>
+      <p className="text-gray-600 mt-2">Page Underdevelopment</p>
     </div>
   );
 }
